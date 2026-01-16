42658010f6b4c88bd02f66c3d1904ec12b8774b6
Bangash <urzali@hotmail.com>
2026-01-16 14:43:44 -0600

    Enhance RestartContainer.sh with usage comments
    Added comments to clarify script usage with cronjob.

--- RestartContainer.sh
+++ RestartContainer.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Simple script that can be used under cronjob to restart container if it is down
+# crontab command to use: */5  * * * * /app/scripts/servicesrestart.sh
+
 sudo docker ps | grep filebeat > /app/tmp/cronresults01
 sudo docker ps | grep syslog-ng > /app/tmp/cronresults02
 
